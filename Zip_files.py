5311df6a7d826e1a9d7520bb2ff821bafaf10ff9
Haraprasanna Misra <haraprasannamisra@gmail.com>
2023-06-22 02:58:05 +0530

    Fixed a bug in Script.py file wherein the core application asumed that the source and destination is the same folder where script.py file is located. Added modularity by introducing functions

---
+++ Zip_files.py
@@ -0,0 +1,9 @@
+import zipfile
+import os
+
+def createZip():
+    with zipfile.ZipFile("Zipped videos.zip","w") as handle:
+        for x in os.listdir():
+            fileNameAerray = x.split()
+            if len(fileNameAerray) == 5 and fileNameAerray[1] == "(part":
+                handle.write(x, compress_type=zipfile.ZIP_DEFLATED)
